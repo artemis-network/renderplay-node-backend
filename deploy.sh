ca18295257b71fbfc986e3bdf703c96064163e16
akashmadduru <akashmadduru@gmail.com>
2022-06-21 16:00:47 +0530

    fix refactoring

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@ docker stop renderverse-backend-server
 docker rm renderverse-backend-server
 docker rmi renderverse/backend-server
 docker build --no-cache -t renderverse/backend-server .
-docker run -p 5000:5000 --restart=always --name renderverse-backend-server -d --env-file .env renderverse/backend-server
+docker run -p 5000:5000 -p 587:587 --restart=always --name renderverse-backend-server -d --env-file .env renderverse/backend-server
 docker system prune -a -f
 docker ps -a
